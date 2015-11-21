96ac34c19f02c4de55f4b2955be898f184656052
Dmitry Bardt <dmitrybardt@coherentsolutions.com>
2015-11-21 16:55:24 +0300

    Added mathService with 2 func distance

--- task1/app/Main.hs
+++ task1/app/Main.hs
@@ -1,16 +1,25 @@
 module Main where
 
+import MathService
+import System.IO
+import System.IO.Error
 import Data.Conduit
 import Data.Conduit.Binary
 import Data.CSV.Conduit
 import Data.Text (Text)
-import qualified Data.ByteString as byteString
+import qualified Data.ByteString as B
 
--- | File where the CSV is stored.
+-- | File for tested
 csvFile :: FilePath
 csvFile = "resources/glass.txt"
 
 main :: IO ()
-main = do
-	file <- byteString.readFile csvFile
-	putStrLn "reading file"
+main = putStrLn $ show $ hammingDistance [1, 2, 3] [2, 3, 1, 5]
+-- main = do
+-- 	putStrLn "Please, write input.csv file:"
+-- 	csvFile <- getLine
+-- 	putStrLn "Count clasters:"
+-- 	countClaster <- getLine
+-- 	putStrLn "Write epsilon:"
+-- 	epsilon <- getLine
+-- 	putStrLn "Thanks"
---
+++ task1/src/MathService.hs
@@ -0,0 +1,14 @@
+module MathService
+    ( hammingDistance,
+      euclideanDistance
+    ) where
+
+hammingDistance :: [Double] -> [Double] -> Int
+hammingDistance listX listY = sum $ zipWith diff listX listY
+                where diff a b | a == b = 0
+                               | otherwise = 1
+
+euclideanDistance :: [Double] -> [Double] -> Double
+euclideanDistance listX listY = sqrt $ sum $ zipWith diff listX listY
+                where diff a b | a == b = 0
+                               | otherwise = 1
