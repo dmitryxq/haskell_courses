0c16954968f8271409c373946c97d973d9e327f6
Dmitry Bardt <dmitrybardt@coherentsolutions.com>
2015-12-03 16:43:34 +0300

    complete parsing

--- task1/src/ParserService.hs
+++ task1/src/ParserService.hs
@@ -12,46 +12,46 @@ import Data.Vector as V
 
 import qualified Data.ByteString.Lazy as BL
 
+-- public
 parserCSVFile :: ByteString -> ParserCSVOption -> Either String (Vector (Vector Double))
 parserCSVFile contents options = 
     let ParserCSVOption {splitterColumn = splitter} = options
         ParserCSVOption {ignoreHeader = ignoreHeader} = options
         decodeOptions = DecodeOptions { decDelimiter = fromIntegral (ord splitter) }
 
-        resultData = deleteHeader contents decodeOptions ignoreHeader
+        -- delete header if needed
+        resultData = deleteHeader contents decodeOptions ignoreHeader 
 
-    in decodingData resultData
+    in decodingData resultData options
+
+-- private
+decodingData :: Either String (Vector (Vector String)) -> ParserCSVOption -> Either String (Vector (Vector Double))  
+decodingData infoData options = case infoData of 
+    Left errorMessage -> Left errorMessage
+    Right parsedData -> Right (V.map (V.map read) (deleteLastColumn options (deleteFirstColumn options parsedData)))
 
 
+-- private
 deleteHeader :: ByteString -> DecodeOptions -> Bool -> Either String (Vector (Vector String))
 deleteHeader contents decodeOptions ignoreHeader = 
     if ignoreHeader
         then decodeWith decodeOptions HasHeader contents
     else decodeWith decodeOptions NoHeader contents
 
+-- private
+deleteFirstColumn :: ParserCSVOption -> Vector(Vector String) -> Vector(Vector String)
+deleteFirstColumn options contents = 
+    let ParserCSVOption {ignoreFirstColumn = ignoreColumn} = options
+    in    
+    if ignoreColumn
+        then V.map V.init contents
+    else contents 
 
-decodingData :: Either String (Vector (Vector String)) -> Either String (Vector (Vector Double))  
-decodingData infoData = case infoData of Left errorMessage -> Left errorMessage
-                                         Right parsedData -> Right (V.map (V.map read) parsedData)
-
--- parser :: ParserCSVOption -> String -> [[String]]
--- parser opts s = 
---     let rows = if ignoreHeader opts then deleteNewLinesWithoutHeader s else deleteNewLines s
---         getRow = case (ignoreFirstColumn opts, ignoreLastColumn opts) of
---                      (True, True) -> init . tail . splitOn splitterColumn opts
---                      (True, False) -> tail . splitOn splitterColumn opts
---                      (False, True) -> init . splitOn splitterColumn opts
---                      (False, False) -> splitOn splitterColumn opts
---     in
--- 	[getRow line | line <- rows]
-
--- parseCsv :: FilePath -> ParserCSVOption -> IO [[String]]
--- parseCsv f opts = do
---     s <- readFile f
---     return (parser opts s)
-
--- deleteNewLines :: String -> [String]  
--- deleteNewLines str = lines str  
-
--- deleteNewLinesWithoutHeader :: String -> [String]  
--- deleteNewLinesWithoutHeader str = tail lines str  
+-- private
+deleteLastColumn :: ParserCSVOption -> Vector(Vector String) -> Vector(Vector String)
+deleteLastColumn options contents = 
+    let ParserCSVOption {ignoreLastColumn = ignoreColumn} = options
+    in    
+    if ignoreColumn
+        then V.map V.init contents
+    else contents 
